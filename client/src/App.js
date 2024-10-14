82b838c9cfbe7ddd940a7e5383f8db45937b129f
shannon-jx <shannonngjx@gmail.com>
2024-10-13 21:49:12 -0500

    feat: init response results

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import FAQ from './sections/FAQ/FAQ';
 import Login from './sections/Login/Login';
 import Register from './sections/Register/Register';
 import CreateForm from './sections/CreateForm/CreateForm';
+import Responses from './sections/ResponseAnalysis/Responses';
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/create-form" element={<CreateForm />} />
+          <Route path="/form/:formId" element={<Responses />} />
         </Routes>
       </div>
     </Router>
