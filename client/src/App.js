1f2cea4b6a265b4cacd710fe7c7d4b05f4df1fff
shannon-jx <shannonngjx@gmail.com>
2024-11-04 23:20:01 -0600

    fix: add useLocation

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Header from './sections/Header/Header';
 import Home from './sections/Home/Home';
 import Features from './sections/Features/Features';
